db2a7298c662ddc6ca7393d5d7534b8f06a66161
Brian Kwok <brkwok@gmail.com>
2023-04-30 16:41:56 -0400

    Init neural network class

---
+++ NeuralNetwork.py
@@ -0,0 +1,16 @@
+# Neural Network class def
+class NeuralNetwork:
+    def __init__(self, inputNodes: int, hiddenNodes: int, outputNodes: int, learningRate: float) -> None:
+        # nodes for the neural network
+        self.inodes = inputNodes
+        self.hnodes = hiddenNodes
+        self.onodes = outputNodes
+
+        # learning rate
+        self.lr = learningRate
+
+    def train():
+        pass
+    
+    def query():
+        pass
